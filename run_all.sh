5f086d9d3d9e9647f2f8d4d6f2504068ab08a3a2
alexandermichels <alexandercm4297@gmail.com>
2018-09-14 12:01:22 -0400

    Here is the output good sir, it took forever, but finished. I talked to my stats prof about tf-idf and she gave me some ideas

--- run_all.sh
+++ run_all.sh
@@ -1,30 +1,12 @@
 #!/bin/bash
-python -m streamminer2 -d datasets/sample.csv -o output -m sm
-sleep 60
-python -m streamminer -d datasets/sample.csv -o output -m klinker
-sleep 60
-python -m streamminer -d datasets/sample.csv -o output -m stream
-sleep 60
-python -m streamminer -d datasets/sample.csv -o output -m predpath
-sleep 60
-python -m streamminer -d datasets/sample.csv -o output -m relklinker
-sleep 60
-python -m streamminer -d datasets/real/place_of_birth.csv -o output -m stream
-sleep 60
-python -m streamminer -d datasets/real/place_of_birth.csv -o output -m klinker
-sleep 60
-python -m streamminer -d datasets/real/place_of_birth.csv -o output -m predpath
-sleep 60
-python -m streamminer -d datasets/real/place_of_birth.csv -o output -m relklinker
-sleep 60
-python -m streamminer2 -d datasets/real/place_of_birth.csv -o output -m sm
-sleep 60
-python -m streamminer -d datasets/synthetic/Player_vs_Team_NBA.csv -o output -m stream
-sleep 60
-python -m streamminer -d datasets/synthetic/Player_vs_Team_NBA.csv -o output -m klinker
-sleep 60
-python -m streamminer -d datasets/synthetic/Player_vs_Team_NBA.csv -o output -m predpath
-sleep 60
-python -m streamminer -d datasets/synthetic/Player_vs_Team_NBA.csv -o output -m relklinker
-sleep 60
-python -m streamminer2 -d datasets/synthetic/Player_vs_Team_NBA.csv -o output -m sm
+declare -a methods=( "klinker" "sm" "relklinker" "stream" "predpath" )
+declare -a datasets=( "datasets/sample.csv" "datasets/synthetic/Player_vs_Team_NBA.csv" "datasets/real/place_of_death.csv" )
+
+for i in "${methods[@]}"
+do
+    for j in "${datasets[@]}"
+    do
+        python -m streamminer2 -d $j -o output -m $i
+        sleep 60
+    done
+done
