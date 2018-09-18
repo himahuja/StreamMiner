8091a27e9e46aedb007c2bdfda484aa9a1439000
alexandermichels <alexandercm4297@gmail.com>
2018-09-18 18:40:09 -0400

    Lots of logs?

--- run_all_sm.sh
+++ run_all_sm.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-declare -a methods=( "sm" )
 declare -a datasets=( "datasets/synthetic/birthplace_deathplace.csv" "datasets/real/place_of_birth.csv" "datasets/synthetic/cross_Movies_vs_Directors.csv" "datasets/sample.csv" "datasets/real/derived_filtered_nationality_train_25facts.csv" "datasets/synthetic/cross_US_Presidents_vs_First_Lady.csv" "datasets/real/derived_filtered_profession_train_25facts.csv" "datasets/synthetic/predpath_civil_war_battle.csv" "datasets/real/education_degree.csv" "datasets/synthetic/predpath_company_president.csv" "datasets/real/institution.alm.csv" "datasets/sub_sample.csv" "datasets/synthetic/predpath_state_capital.csv" "datasets/sample.csv" "datasets/synthetic/Player_vs_Team_NBA.csv" "datasets/real/place_of_death.csv" "datasets/synthetic/predpath_vice_president.csv" )
 
-for i in "${methods[@]}"
+for i in "${datasets[@]}"
 do
-    for j in "${datasets[@]}"
-    do
-        python -m streamminer2 -d $j -o output -m $i
-        sleep 60
-    done
+    python -m streamminer2 -d $i -o output -m sm
+    sleep 60
 done
