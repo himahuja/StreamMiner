7b90b66b54775125596a04eb61d4d7b41a0f24f5
alexandermichels <alexandercm4297@gmail.com>
2018-09-30 23:00:44 -0400

    Deleted subsample lots and adding completed logs

--- run_scripts/run_sm.sh
+++ run_scripts/run_sm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ..
-declare -a datasets=( "datasets/synthetic/cross_Movies_vs_Directors.csv" "datasets/real/derived_filtered_nationality_train_25facts.csv"  "datasets/real/derived_filtered_profession_train_25facts.csv" "datasets/real/place_of_death.csv" "datasets/real/place_of_birth.csv" "datasets/real/education_degree.csv" "datasets/real/institution.alm.csv" )
+declare -a datasets=( "datasets/real/education_degree.csv" "datasets/real/institution.alm.csv" )
 
 for i in "${datasets[@]}"
 do
